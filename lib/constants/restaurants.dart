c19b22f0828489a9bc0a03cd46f75f954846eb89
Dydja <kadybayoko16@gmail.com>
2023-03-31 18:45:49 +0000

    first commit

---
+++ lib/constants/restaurants.dart
@@ -0,0 +1,44 @@
+List<Map> restaurants = [
+  {
+    'id': '0',
+    'name': 'Starbucks',
+    'items': 'Coffee, Espresso, Cold Brew, Desserts',
+    'image': 'https://api.time.com/wp-content/uploads/2016/04/starbucks.jpeg',
+    'coordinates': {
+      'latitude': '5.328432087882963',
+      'longitude': '-4.018772372876823',
+    },
+  },
+  // {
+  //   'id': '0',
+  //   'name': 'KFC',
+  //   'items': 'Buckets, Burgers, Biryani, Snacks, Beverages',
+  //   'image':
+  //       'https://indiaeducationdiary.in/wp-content/uploads/2020/10/IMG-20201024-WA0014.jpg',
+  //   'coordinates': {
+  //     'latitude': '37.333556739502136',
+  //     'longitude': '-122.02940574472692',
+  //   },
+  // },
+  // {
+  //   'id': '0',
+  //   'name': 'Domino\'s',
+  //   'items': 'Pizza, Sides, Beverages, Pasta, Chicken',
+  //   'image':
+  //       'https://image.cnbcfm.com/api/v1/image/106334183-1578943648578gettyimages-526250086.jpeg?v=1578943742&w=1600&h=900',
+  //   'coordinates': {
+  //     'latitude': '+5.33915923',
+  //     'longitude': '-4.01841780',
+  //   },
+  // },
+  // {
+  //   'id': '0',
+  //   'name': 'Pizza Hut',
+  //   'items': 'Pizza, Sides, Drinks, Deserts',
+  //   'image': 'https://10619-2.s.cdn12.com/rests/small/w550/h367/103_510330142.jpg',
+  //   'coordinates': {
+  //     'latitude': '+5.33915923',
+  //     'longitude': '-4.01841780',
+  //   },
+  // },
+];
